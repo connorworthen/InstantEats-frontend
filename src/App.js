4c5dc4702fb6444fabd8d46fed30f0a40c1a0275
connorworthen <connorw220@protonmail.com>
2020-12-31 16:01:16 -0800

    test to get data from db

--- src/App.js
+++ src/App.js
@@ -1,6 +1,13 @@
 import React from 'react';
 
-class App extends React.Component () {
+class App extends React.Component {
+
+  componentDidMount() {
+    fetch('http://localhost:3000/api/v1/restaurants')
+    .then(resp => resp.json())
+    .then(data => console.log(data))
+  }
+
   render() {
     return (
       <div className="App">
