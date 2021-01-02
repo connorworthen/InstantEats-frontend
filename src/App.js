37fb4a9d0bbb86cc2cc0a2bed3d7b8f327e5538e
connorworthen <connorw220@protonmail.com>
2021-01-02 13:22:33 -0800

    signup/login creation

--- src/App.js
+++ src/App.js
@@ -1,20 +1,70 @@
-import React from 'react';
-
-class App extends React.Component {
-
-  componentDidMount() {
-    fetch('http://localhost:3000/api/v1/restaurants')
-    .then(resp => resp.json())
-    .then(data => console.log(data))
+import React, { Component } from 'react';
+import axios from 'axios'
+import {BrowserRouter, Switch, Route} from 'react-router-dom'
+import Home from './components/Home'
+import Login from './components/registrations/Login'
+import Signup from './components/registrations/Signup'
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { 
+      isLoggedIn: false,
+      user: {}
+    };
   }
-
-  render() {
+componentDidMount() {
+    this.loginStatus()
+  }
+  loginStatus = () => {
+    axios.get('http://localhost:3001/logged_in', {withCredentials: true})
+    .then(response => {
+      if (response.data.logged_in) {
+        this.handleLogin(response)
+      } else {
+        this.handleLogout()
+      }
+    })
+    .catch(error => console.log('api errors:', error))
+  }
+handleLogin = (data) => {
+    this.setState({
+      isLoggedIn: true,
+      user: data.user
+    })
+  }
+handleLogout = () => {
+    this.setState({
+    isLoggedIn: false,
+    user: {}
+    })
+  }
+render() {
     return (
-      <div className="App">
-        app 
+      <div>
+        <BrowserRouter>
+          <Switch>
+            <Route 
+              exact path='/' 
+              render={props => (
+              <Home {...props} loggedInStatus={this.state.isLoggedIn}/>
+              )}
+            />
+            <Route 
+              exact path='/login' 
+              render={props => (
+              <Login {...props} handleLogin={this.handleLogin} loggedInStatus={this.state.isLoggedIn}/>
+              )}
+            />
+            <Route 
+              exact path='/signup' 
+              render={props => (
+              <Signup {...props} handleLogin={this.handleLogin} loggedInStatus={this.state.isLoggedIn}/>
+              )}
+            />
+          </Switch>
+        </BrowserRouter>
       </div>
     );
   }
 }
-
 export default App;
