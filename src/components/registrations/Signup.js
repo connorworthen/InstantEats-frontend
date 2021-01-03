c08f88410c13b998726cda07278a4b9504a0d10b
connorworthen <connorw220@protonmail.com>
2021-01-02 21:54:11 -0800

    user can signup & saves on backend

--- src/components/registrations/Signup.js
+++ src/components/registrations/Signup.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 
 class Signup extends Component {
 
@@ -10,43 +11,39 @@ class Signup extends Component {
       email: '',
       password: '',
       address: '',
-      phone_number: ''
+      phone_number: '',
+      signupErrors: ''
     };
-
-    this.handleSubmit = this.handleSubmit.bind(this)
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleChange = this.handleChange.bind(this);
   }
 
-  handleChange = (event) => {
-    const {name, value} = event.target
+  handleChange(event) {
     this.setState({
-      [name]: value
+      [event.target.name]: event.target.value
     })
-  };
+  }
 
-  handleSubmit(e) {
-    e.preventDefault()
-    const {first_name, last_name, email, password, address, phone_number} = this.state
-    let user = {
-      first_name: first_name,
-      last_name: last_name,
-      email: email,
-      password: password,
-      address: address,
-      phone_number: phone_number
-    }
-    return fetch('http://localhost:3001/api/v1/users', {
-      method: "POST",
-      body: user
+  handleSubmit(event) {
+    const { first_name, last_name, email, password, address, phone_number} = this.state;
+    axios.post("http://localhost:3000/api/v1/users", {
+      user: {
+        first_name: first_name,
+        last_name: last_name,
+        email: email,
+        password: password,
+        address: address,
+        phone_number: phone_number
+      }
+    }, 
+    { withCredentials: true }
+    ).then(response => {
+      console.log("response", response)
     })
-    .then(response => {
-
-        this.props.handleLogin(response.data)
-        this.redirect()
+    .catch(error => {
+      console.log("response", error)
     })
-  };
-
-  redirect = () => {
-    this.props.history.push('/')
+    event.preventDefault();
   }
 
 render() {
@@ -55,58 +52,55 @@ render() {
         <h1>Sign Up</h1>
       <form onSubmit={this.handleSubmit}>
           <input
-            placeholder="first_name"
-            type="text"
+            type="first_name"
             name="first_name"
+            placeholder="First Name"
             value={this.state.first_name}
             onChange={this.handleChange}
             required
           />
           <input
-            placeholder="last_name"
-            type="text"
+            type="last_name"
             name="last_name"
+            placeholder="Last Name"
             value={this.state.last_name}
             onChange={this.handleChange}
             required
           />
           <input 
-            placeholder="email"
             type="email"
             name="email"
+            placeholder="Email"
             value={this.state.email}
             onChange={this.handleChange}
             required
           />
           <input
-            placeholder="password"
             type="password"
             name="password"
+            placeholder="Password"
             value={this.state.password}
             onChange={this.handleChange}
             required
           />
           <input
-            placeholder="address"
-            type="text"
+            type="address"
             name="address"
+            placeholder="Address"
             value={this.state.address}
             onChange={this.handleChange} 
             required
           />
-          <input 
-            placeholder="phone_number"
+          <input
             type="phone_number"
             name="phone_number"
+            placeholder="Phone Number"
             value={this.state.phone_number}
             onChange={this.handleChange}
             required
           />
 
-
-          <button placeholder="submit" type="submit">
-            Sign Up
-          </button>
+          <button type="submit">Create Account</button>
 
         </form>  
       </div>
