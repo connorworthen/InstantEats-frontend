519dde037b6b532614e8b2eec0c78fe372eced7a
connorworthen <connorw220@protonmail.com>
2021-01-04 14:42:35 -0800

    rendering components through usercontainer

--- src/actions/fetchUsers.js
+++ src/actions/fetchUsers.js
@@ -2,4 +2,7 @@
 
 export function fetchUsers() {
 
+  // fetch('http://localhost:3001/api/v1/users')
+  // .then(resp => resp.json())
+  // .then(data => console.log(data))
 } 
--- src/containers/UsersContainer.js
+++ src/containers/UsersContainer.js
@@ -1,11 +1,15 @@
 import React from 'react';
+import {connect} from 'react-redux'
+import Users from '../components/Users'
+import UserInput from '../components/UserInput'
 
 class UsersContainer extends React.Component {
 
   render() {
     return (
       <div>
-        UsersContainer
+        <UserInput/>
+        <Users/>
       </div>
     )
   }
