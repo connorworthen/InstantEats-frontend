82ea004cbc1a383ab497d7c9061ba540799a733d
connorworthen <connorw220@protonmail.com>
2021-01-03 20:12:24 -0800

    setting up store

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
 import React from 'react';
-// import { BrowserRouter, Switch} from 'react-router-dom';
 
 class App extends React.Component {
 
   render() {
     return (
       <div className="app">
-        
+        App
       </div>
     )
   }
