fccca1a411774724acf512516eb7fb6e7a044585
connorworthen <connorw220@protonmail.com>
2021-01-04 13:51:59 -0800

    fetching users

--- src/App.js
+++ src/App.js
@@ -1,6 +1,14 @@
 import React from 'react';
+import {connect} from 'react-redux';
+import {fetchUsers} from './actions/fetchUsers';
 
 class App extends React.Component {
+
+  componentDidMount() {
+    // fetch('http://localhost:3001/api/v1/users')
+    // .then(resp => resp.json())
+    // .then(data => console.log(data))
+  }
 
   render() {
     return (
@@ -11,4 +19,11 @@ class App extends React.Component {
   }
 }
 
-export default App;
+// const mapStateToProps = (state) => {
+//   return {
+//     users: state.users
+//   }
+// }
+
+
+export default connect(null, {fetchUsers})(App);
