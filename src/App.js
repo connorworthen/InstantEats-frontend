f99a97e6b24c35ed601031fb04133fedd303e7e5
connorworthen <connorw220@protonmail.com>
2021-01-03 19:26:40 -0800

    restarting

--- src/App.js
+++ src/App.js
@@ -1,52 +1,15 @@
-import React, { Component } from 'react';
-import { BrowserRouter, Switch, Route } from 'react-router-dom';
-import UserContainer from './containers/userContainer.js'
-import {connect} from 'react-redux'
-import {fetchLoggedInUser} from './fetchRequestss/fetchUser'
-import {logOutUser} from './actions/userAction'
+import React from 'react';
+// import { BrowserRouter, Switch} from 'react-router-dom';
 
 class App extends React.Component {
-
-  componentDidMount(){
-    this.fetchEverything()   
-  }
-
-  fetchEverything = () => { 
-    this.props.fetchLoggedInUser()
-  }
-
-  logOut = ()=>{
-    localStorage.removeItem("token")
-    this.props.logOutUser()
-    alert("Succefully log out!")
-  }
 
   render() {
     return (
       <div className="app">
-        <BrowserRouter>
-          <Switch>
-            <Route exact path="/login" component={UserContainer} />
-            <Route exact path="/signup" render={props=><UserContainer {...props} />} />
-          </Switch>
-        </BrowserRouter>
+        hello
       </div>
     )
   }
 }
 
-const mapStateToProps = state =>{
-  return{
-    login: state.user.login
-  }
-}
-
-
-const mapDispatchToProps = dispatch =>{
-  return{
-    fetchLoggedInUser: () => dispatch(fetchLoggedInUser()),
-    logOutUser: ()=>dispatch(logOutUser())
-  }
-}
-
-export default connect(mapStateToProps, mapDispatchToProps)(App);
+export default App;
