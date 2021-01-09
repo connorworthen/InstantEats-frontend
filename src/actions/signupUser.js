37382f4115ee5018eaaac94be9f44ad21d054ddf
connorworthen <connorw220@protonmail.com>
2021-01-08 20:34:28 -0800

    action reaches backend

--- src/actions/signupUser.js
+++ src/actions/signupUser.js
@@ -1,7 +1,12 @@
 
 export const signupUser = (data) => {
-  debugger
   return (dispatch) => {
-
+    fetch('http://localhost:3001/api/v1/users', {
+      headers: {
+        'Content-type': 'application/json',
+      },
+      method: 'POST',
+      body: JSON.stringify(data)
+    })
   }
 }
