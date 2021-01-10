f122dfab123f589072607832c7d5179c789f56d9
connorworthen <connorw220@protonmail.com>
2021-01-09 16:18:42 -0800

    users added to state without refresh

--- src/actions/signupUser.js
+++ src/actions/signupUser.js
@@ -1,13 +1,15 @@
 
 export const signupUser = (data) => {
-  debugger
   return (dispatch) => {
     fetch('http://localhost:3001/api/v1/users', {
       headers: {
-        'Content-type': 'application/json',
+        'Content-Type': 'application/json',
+        'Accept': 'application/json'
       },
       method: 'POST',
       body: JSON.stringify(data)
     })
+    .then(resp => resp.json())
+    .then(user => dispatch({type: 'ADD_USER', payload: user}))
   }
 }
