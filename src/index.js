825de079c3a4cda14dd103ea734c4b202b6bb0a8
connorworthen <connorw220@protonmail.com>
2021-01-10 15:42:20 -0800

    importing browser router/wrapping app

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import {createStore, applyMiddleware, compose} from 'redux';
 import thunk from 'redux-thunk';
 import { Provider } from 'react-redux';
+import { BrowserRouter as Router } from 'react-router-dom';
 import userReducer from './reducers/userReducer';
 import App from './App';
 
@@ -12,5 +13,7 @@ let store = createStore(userReducer, composeEnhancers(applyMiddleware(thunk)))
 
 ReactDOM.render(
   <Provider store={store}>
-    <App />
+    <Router>
+      <App />
+    </Router>
   </Provider>, document.getElementById('root'));
