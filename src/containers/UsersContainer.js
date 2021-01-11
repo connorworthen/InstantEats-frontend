8ae3b88d57b9a7a35573f9389f9428253424b50c
connorworthen <connorw220@protonmail.com>
2021-01-10 16:09:08 -0800

    setting up routes

--- src/containers/UsersContainer.js
+++ src/containers/UsersContainer.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import {connect} from 'react-redux'
+import {Route} from 'react-router-dom'
 import {fetchUsers} from '../actions/fetchUsers'
 import Users from '../components/Users'
 import UserInput from '../components/UserInput'
@@ -9,11 +10,13 @@ class UsersContainer extends React.Component {
   componentDidMount() {
     this.props.fetchUsers()
   }
+
   render() {
     return (
       <div>
-        <UserInput/><br></br>
-        <Users users={this.props.users} />
+        <Route exact path='/users/new' component={UserInput} />
+        <Route exact path='/users' render={() => <Users users={this.props.users} /> }/>
+        Home Page
       </div>
     )
   }
