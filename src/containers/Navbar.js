a7c41caec0b07928e1d858fff304f394d4d69d87
connorworthen <connorw220@protonmail.com>
2021-01-10 20:21:17 -0800

    navbar creation + css folder/files

---
+++ src/containers/Navbar.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+class Navbar extends React.Component {
+  
+  
+  
+  render() {
+    return (
+      <nav className="NavbarItems">
+
+      </nav>
+    )
+  }
+}
+
+export default Navbar
