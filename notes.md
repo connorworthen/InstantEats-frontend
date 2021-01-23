cddf082d2ca0bc6ac6f74e159f98e8d2c652785b
connorworthen <connorw220@protonmail.com>
2021-01-22 16:01:15 -0800

    deleted navbar + updated notes

--- notes.md
+++ notes.md
@@ -1,6 +1,30 @@
-componentDidMount() {
-fetch('http://localhost:3000/api/v1/users')
-.then(resp => resp.json())
-.then(data => console.log(data[0].name))
-}
-connorw23320@protonmai.com
+Tasks:
+
+1. NavBar UI fix.
+   A. Login & Signup Button
+   B. Profile Icon & User name & Logout
+2. Home Page
+   A. Update UI on form + button
+   B. Photo behind bar
+   C. Footer w/ links to github, linkedin, twitter
+3. Restaurant Index
+   A. Fix style boxes
+   B. Attach Images to each restaurant (active models)
+4. Indivdual Restaurant Ids
+   A. Fix display of Restaurant info w/ photo
+   B. Render Menu new UI
+   C. Fix add to cart buttons
+5. Cart UI
+   A. Fix render cart design
+   B. Fix button for delete item
+
+Pratice tests:
+A. Filters (A-Z, Category, Price, etc)
+B. Search Bar by restaurant name
+C. Check over all tested info (write down)
+
+If have time (extra):
+A. Checkout Page post cart
+B. Cart Moved to sidebar update in restaurant/:id
+C. Modal for signup/login
+D. Modal for each menu item
--- src/containers/Navbar.js
+++ src/containers/Navbar.js
@@ -1,8 +1,7 @@
 import React from 'react'
 import {connect} from 'react-redux';
 import {logUserOut} from '../actions/userActions'
-// import * as ReactBootStrap from 'react-bootstrap'
-import {Nav, NavLink, Bars, NavMenu, NavBtn, NavBtnLink } from './NavbarElements'
+import * as ReactBootStrap from 'react-bootstrap'
 
 class Navbar extends React.Component {
 
@@ -12,41 +11,15 @@ class Navbar extends React.Component {
 
   render() {
     return (
-      <Nav>
-        <NavLink to='/' activeStyle>
-          <h1>Instant Eats</h1>
-        </NavLink>
-        <Bars />
-        <NavMenu>
-          <NavLink to='/' activeStyle>
-            Home
-          </NavLink>
-          <NavLink to='/' activeStyle>
-            About
-          </NavLink>
-          <NavLink to='/' activeStyle>
-            Contact Us
-          </NavLink>
-        </NavMenu>
-          {
-            !this.props.userReducer.loggedIn ? 
-            <NavBtn>
-              <NavBtnLink to='/signup'>
-                Signup
-              </NavBtnLink>
-              <NavBtnLink to='/login'>
-                Login
-              </NavBtnLink>
-            </NavBtn> 
-            :
-            <NavBtn> 
-                <NavBtnLink to='/' onClick={this.handleLogout}>
-                  Welcome {this.props.userReducer.user.user.first}
-                  Logout
-                </NavBtnLink>
-            </NavBtn> 
-          }      
-      </Nav>
+      <ReactBootStrap.Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+        <ReactBootStrap.Navbar.Brand href="/">Instant Eats</ReactBootStrap.Navbar.Brand>
+        {
+          !this.props.userReducer.loggedIn ? 
+        <div><ReactBootStrap.Nav.Link href="/signup">Sign up</ReactBootStrap.Nav.Link>
+        <ReactBootStrap.Nav.Link href="/login">Login</ReactBootStrap.Nav.Link></div> : 
+        <div><h1>Welcome {this.props.userReducer.user.user.first}</h1> <ReactBootStrap.Nav.Link onClick={this.handleLogout}>Logout</ReactBootStrap.Nav.Link></div>
+        }
+      </ReactBootStrap.Navbar> 
     )
   }
 }
@@ -59,12 +32,3 @@ const mapStateToProps = (state) => {
 
 export default connect(mapStateToProps, {logUserOut})(Navbar)
 
-// <ReactBootStrap.Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
-//         <ReactBootStrap.Navbar.Brand href="/">Instant Eats</ReactBootStrap.Navbar.Brand>
-//         {
-//           !this.props.userReducer.loggedIn ? 
-//         <div><ReactBootStrap.Nav.Link href="/signup">Sign up</ReactBootStrap.Nav.Link>
-//         <ReactBootStrap.Nav.Link href="/login">Login</ReactBootStrap.Nav.Link></div> : 
-//         <div><h1>Welcome {this.props.userReducer.user.user.first}</h1> <ReactBootStrap.Nav.Link onClick={this.handleLogout}>Logout</ReactBootStrap.Nav.Link></div>
-//         }
-//       </ReactBootStrap.Navbar> 
