cf98c699f721c2eb47e1e976c630cab1d9e8680a
connorworthen <connorw220@protonmail.com>
2021-03-19 17:22:19 -0700

    redoing nav bar

--- src/containers/Navbar.js
+++ src/containers/Navbar.js
@@ -1,8 +1,10 @@
 import React from 'react'
 import {connect} from 'react-redux';
 import {logUserOut} from '../actions/userActions'
+import {Link} from 'react-router-dom'
 // import {GoogleLogout} from 'react-google-login';
-import * as ReactBootStrap from 'react-bootstrap'
+// import * as ReactBootStrap from 'react-bootstrap'
+import '../nav.css'
 
 class Navbar extends React.Component {
 
@@ -12,17 +14,26 @@ class Navbar extends React.Component {
 
   render() {
     return (
-      <ReactBootStrap.Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
-        <ReactBootStrap.Navbar.Brand href="/">Instant Eats</ReactBootStrap.Navbar.Brand>
-        {
-          !this.props.userReducer.loggedIn ? 
-        <div><ReactBootStrap.Nav.Link href="/signup">Sign up</ReactBootStrap.Nav.Link>
-        <ReactBootStrap.Nav.Link href="/login">Login</ReactBootStrap.Nav.Link>
-        </div>
-        : 
-        <div><h1>Welcome, {this.props.userReducer.user.profileObj} </h1><ReactBootStrap.Nav.Link onClick={this.handleLogout}>Logout</ReactBootStrap.Nav.Link></div>
-        }
-      </ReactBootStrap.Navbar> 
+      <div>
+        <header class="nav-bar">
+          <ul class="nav-bar-items">
+            <li>
+              <a href="#/">
+                <div class="nav-logo">
+                  <img src="https://seat-check-seeds.s3-us-west-1.amazonaws.com/sleeper-chair.png" />
+                  <h1 class="logo">Instant Eats</h1>
+                </div>
+              </a>
+            </li>
+            <li>
+              <nav class="greeting-links">
+                <Link to="/signup" class="form-button-signup nav-button">Sign up</Link>
+                <button class="form-button-signin nav-button">Sign in</button>
+              </nav>
+            </li>
+          </ul>
+        </header>
+      </div>
     )
   }
 }
@@ -37,4 +48,14 @@ export default connect(mapStateToProps, {logUserOut})(Navbar)
 
 // : {this.props.userReducer.user.user.first}
 
-// if user signed in through google clear google local storage & signout else if user signed in w/o 3rd party clear local storage
+{/* <ReactBootStrap.Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+        <ReactBootStrap.Navbar.Brand href="/">Instant Eats</ReactBootStrap.Navbar.Brand>
+        {
+          !this.props.userReducer.loggedIn ? 
+        <div><ReactBootStrap.Nav.Link href="/signup">Sign up</ReactBootStrap.Nav.Link>
+        <ReactBootStrap.Nav.Link href="/login">Login</ReactBootStrap.Nav.Link>
+        </div>
+        : 
+        <div><h1>Welcome, {this.props.userReducer.user.profileObj} </h1><ReactBootStrap.Nav.Link onClick={this.handleLogout}>Logout</ReactBootStrap.Nav.Link></div>
+        }
+      </ReactBootStrap.Navbar>  */}
